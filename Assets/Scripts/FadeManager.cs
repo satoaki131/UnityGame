ae09fe38caa2828201e1fa392f3392b69c155bd7
Satoaki131 <akinori_0131@yahoo.co.jp>
2016-03-26 15:48:58 +0900

    FadeManager追加

---
+++ Assets/Scripts/FadeManager.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class FadeManager : MonoBehaviour {
+
+    static GameObject _instance = null;
+
+    private float _alpha = 0.0f;
+    private Image _fadeObject = null;
+
+    private static bool _fadeFlug = false;
+    //public bool isFadeFlug
+    //{
+    //    get { return _fadeFlug; }
+    //    set { _fadeFlug = value; }
+    //}
+
+    void Awake()
+    {
+        if(_instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            _instance = gameObject;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        _fadeObject = GetComponentInChildren<Image>();
+        _fadeObject.color = new Color(0.0f, 0.0f, 0.0f, _alpha);
+
+    }
+    
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0) && !_fadeFlug)
+        {
+            _fadeFlug = true;
+            StartCoroutine(Fade(GameScene.GAME));
+        }
+    }
+
+    public IEnumerator Fade(GameScene scene)
+    {
+        while(_alpha < 1.0f)
+        {
+            FadeIn();
+            yield return null;
+        }
+
+        FadeAdjustment(1.0f);
+        SceneManager.LoadScene((int)scene);
+
+        while (_alpha > 0.0f)
+        {
+            FadeOut();
+            yield return null;
+        }
+
+        FadeAdjustment(0.0f);
+        _fadeFlug = false;
+        yield return null;
+    }
+
+    private void FadeIn()
+    {
+        _fadeObject.color = new Color(0.0f, 0.0f, 0.0f, _alpha);
+        _alpha += 0.01f;
+    }
+
+    private void FadeOut()
+    {
+        _fadeObject.color = new Color(0.0f, 0.0f, 0.0f, _alpha);
+        _alpha -= 0.01f;
+    }
+
+    private void FadeAdjustment(float alpha)
+    {
+        _alpha = alpha;
+        _fadeObject.color = new Color(0.0f, 0.0f, 0.0f, _alpha);
+    }
+}
---
+++ Assets/Scripts/GameScene.cs
@@ -0,0 +1,10 @@
+﻿
+/// <summary>
+/// シーンの名前を順に追加していく。
+/// ビルドインデックスの順番と数字を合致させること
+/// </summary>
+public enum GameScene
+{
+    TITLE, //0
+    GAME //1
+}
