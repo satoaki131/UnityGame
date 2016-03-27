6029fde55b95ecf8d9659b0b32f593d8be4fec60
Satoaki131 <akinori_0131@yahoo.co.jp>
2016-03-27 12:39:34 +0900

    AudioManager追加

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioManager : MonoBehaviour {
+
+    private static GameObject _instance = null;
+
+    [SerializeField]
+    private AudioClip[] _clip;
+
+    private AudioSource _source;
+    private int _index = 0;
+
+    void Awake()
+    {
+        if(_instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            _instance = gameObject;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        _source = GetComponentInChildren<AudioSource>();
+    }
+
+
+    public void Play(int index, bool loop = false, float volume = 1.0f)
+    {
+        _source.clip = _clip[index];
+        _source.loop = loop;
+        _source.volume = volume;
+        _source.Play();
+    }
+}
